eb22d57edee9e4f1d2719b0d6ad70f2183eb2137
Nikolai-Doroshenko <doroshenko.nikolai.a@gmail.com>
2023-03-02 14:11:17 +0300

    add taskStories

---
+++ src/stories/Task.stories.tsx
@@ -0,0 +1,55 @@
+import React, {ChangeEvent, useState} from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import Task from "../Task";
+import {action} from "@storybook/addon-actions";
+
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: 'TodoList/Task',
+  component: Task,
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+  args: {
+    removeTask: action('removeTask'),
+    changeTaskStatus: action('changeTaskStatus'),
+    changeTaskTitle: action('changeTaskTitle'),
+    todolistId: '1234dsrfew',
+    task: {id: 'afewqrewq', title: 'JS', isDone: false},
+  }
+} as ComponentMeta<typeof Task>;
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template: ComponentStory<typeof Task> = (args) => <Task {...args} />;
+
+export const TaskIsNotDoneStory = Template.bind({});
+
+
+export const TaskIsDoneStory = Template.bind({});
+
+TaskIsDoneStory.args = {
+  task: {id: 'afewqrewq', title: 'React', isDone: true},
+}
+
+
+
+const Template1: ComponentStory<typeof Task> = (args) => {
+  const  [task, setTask] = useState({id: 'aaa', title: 'TypeScript', isDone: false})
+
+  function changeTaskStatus (taskId: string, isDone: boolean)  {
+    setTask({id: 'aaa', title: 'TypeScript', isDone: !task.isDone})
+  }
+  function changeTaskTitle (taskId: string, newTitle: string) {
+    setTask({id: 'aaa', title: newTitle, isDone: false})
+  }
+function removeTask () {
+    console.log('remove task')
+}
+  return <Task
+      task={task}
+      removeTask={removeTask}
+      changeTaskTitle={changeTaskTitle}
+      changeTaskStatus={changeTaskStatus}
+  />
+};
+
+export const TaskStory = Template1.bind({});
